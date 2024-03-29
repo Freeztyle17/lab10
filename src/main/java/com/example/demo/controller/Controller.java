039511e128fed3dd4dc1d23dc3c6853f98661c5e
Ряснов Павел Иванович <bloodstar215@gmail.com>
2024-03-29 12:19:28 +0400

    efd

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -5,12 +5,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/api")
 public class Controller {
     public Controller() {
     }
 
-    @GetMapping("/api/hello")
+    @GetMapping("/hello")
     public ResponseEntity<String> work(){
         return new ResponseEntity<>("2002", HttpStatus.OK);
     }
